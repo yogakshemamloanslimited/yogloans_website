92fd2462f12f260a03352e306de01adc01c4f1fa
anas0022 <anasfamilyman@gmail.com>
2025-09-22 17:30:04 +0530

    up

--- Controllers/AuctionController.cs
+++ Controllers/AuctionController.cs
@@ -1,16 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using yogloansdotnet.Models;
+using yogloansdotnet.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace yogloansdotnet.Controllers
 {
     public class AuctionController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
+        {    var vm = new LoanGroupViewModel
+    {
+        Gold = _context.Homwelcome.Where(x => x.LoanType == "Gold").ToList(),
+        Business = _context.Homwelcome.Where(x => x.LoanType == "Business").ToList(),
+        Vehicle = _context.Homwelcome.Where(x => x.LoanType == "Vehicle").ToList(),
+        CD = _context.Homwelcome.Where(x => x.LoanType == "CD").ToList(),
+        AboutContent = _context.AboutContent.ToList()
+    };
+            return View(vm);
         }
-        public IActionResult LoginPage()
+private readonly ILogger<AuctionController> _logger;
+    private readonly ApplicationDbContext _context;
+    private readonly IWebHostEnvironment _webHostEnvironment;
+            public AuctionController(ILogger<AuctionController> logger, ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
-            return View ("~/Views/Auction/login.cshtml");
+            _logger = logger;
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
+public IActionResult Welcome()
+{
+   
+
+    return View("~/Views/Auction/welcome.cshtml");
+}
+
+
     }
 }
