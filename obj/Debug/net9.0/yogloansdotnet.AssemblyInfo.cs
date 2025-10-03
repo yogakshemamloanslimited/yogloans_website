5bbda6b27c1a6048022085080f6dc51db5a25d63
anas0022 <anasfamilyman@gmail.com>
2025-10-03 15:57:06 +0530

    up

--- obj/Debug/net9.0/yogloansdotnet.AssemblyInfo.cs
+++ obj/Debug/net9.0/yogloansdotnet.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("yogloansdotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdc380de4994114592af3bc8307456c743aebc90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68ece63c2097eca9a1b1e79e3ce39724fc3db06c")]
 [assembly: System.Reflection.AssemblyProductAttribute("yogloansdotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("yogloansdotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
